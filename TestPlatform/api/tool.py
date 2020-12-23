075a15a9a2f2a3db9719062a1f2d001cb260f2bc
yinhang <hang.yin@biomind.ai>
2020-12-23 16:16:12 +0800

    duration bug fix

--- TestPlatform/api/tool.py
+++ TestPlatform/api/tool.py
@@ -345,29 +345,25 @@ class EnableDuration(APIView):
         # 查找id是否存在
         try:
             a = 0
+            nom = 0
             durationid = data["id"]
             obj = duration.objects.get(id=durationid)
             sleepcount = obj.sleepcount if obj.sleepcount is not None else 9999
             sleeptime = obj.sleeptime if obj.sleeptime is not None else 0
 
             if obj.sendcount is None and obj.end_time is None:
-                start = ''
                 end = 1
             elif obj.sendcount is None and obj.end_time is not None:
-                start = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 end = (datetime.datetime.now() + datetime.timedelta(hours=int(obj.end_time))).strftime(
                     "%Y-%m-%d %H:%M:%S")
             else:
-                start =''
-                end = ''
-                nom =0
                 for j in obj.dicom.split(","):
                     nom =nom +1
                 imod = divmod(int(obj.sendcount),nom)
                 if imod[0]< 1:
                     return JsonResponse(code="999994", msg="少于病种数量，请增加发送数量！")
             for i in obj.dicom.split(","):
-                if end =='':
+                if nom!=0:
                     end = int(imod[0]) + int(imod[1]) if a == 0 else int(imod[0])
                 cmd = ('nohup /home/biomind/.local/share/virtualenvs/biomind-dvb8lGiB/bin/python3'
                        ' /home/biomind/Biomind_Test_Platform/TestPlatform/tools/dicom/dicomSend.py '
@@ -376,12 +372,11 @@ class EnableDuration(APIView):
                        '--keyword {3} '
                        '--dicomfolder {4} '
                        '--durationid {5} '
-                       '--start {6} '
-                       '--end {7} '
-                       '--sleepcount {8} '
-                       '--sleeptime {9} '
-                       '--series {10} &').format(obj.server, obj.aet, obj.port, obj.keyword,i, durationid,
-                                             start, end,sleepcount, sleeptime, obj.series)
+                       '--end {6} '
+                       '--sleepcount {7} '
+                       '--sleeptime {8} '
+                       '--series {9} &').format(obj.server, obj.aet, obj.port, obj.keyword,i, durationid,
+                                              end,sleepcount, sleeptime, obj.series)
 
                 logger.info(cmd)
                 os.system(cmd)
